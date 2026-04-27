a5cc114597361e665fefe3c847a918f3f9ee1a01
Bruno Ramos Rodrigues <24brunorodrigues@gmail.com>
2026-04-27 18:11:41 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,11 @@
+const btnPrimary = document.querySelector(".btn-primary");
 const items = document.querySelectorAll(".item");
+
+
+btnPrimary.addEventListener("click", () => {
+  alert("Obrigado! Sua consulta foi agendada. Entraremos em contato em breve!");
+});
+
 
 items.forEach(item => {
   item.querySelector(".item-header").addEventListener("click", () => {
@@ -8,3 +15,4 @@ items.forEach(item => {
     icon.textContent = item.classList.contains("active") ? "−" : "+";
   });
 });
+
